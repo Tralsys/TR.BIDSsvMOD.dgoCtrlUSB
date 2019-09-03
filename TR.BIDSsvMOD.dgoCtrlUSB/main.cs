8d20008a397bb16d5fbb416c0fdc4b5b8d13b6d5
ＴＲ <public@technotter.com>
2019-09-04 01:08:19 +0900

    Add PBExchange Mode

--- TR.BIDSsvMOD.dgoCtrlUSB/main.cs
+++ TR.BIDSsvMOD.dgoCtrlUSB/main.cs
@@ -8,11 +8,14 @@ namespace TR.BIDSsvMOD.dgoCtrlUSB
   {
     public int Version => 100;
     public string Name { get; set; } = "dgoc2bids";
-
     public bool IsDebug { get; set; } = false;
+
+    const int PHandleMAX = 13;
+    const int BHandleMAX = 8;
 
     int? ATCPnlInd = null;
     double MultiplNum = 0;
+    bool isPBExc = false;
 
     public bool Connect(in string args)
     {
@@ -36,6 +39,9 @@ namespace TR.BIDSsvMOD.dgoCtrlUSB
                   break;
                 case "n":
                   Name = saa[1];
+                  break;
+                case "pbexc":
+                  isPBExc = true;
                   break;
               }
             }
@@ -61,10 +67,15 @@ namespace TR.BIDSsvMOD.dgoCtrlUSB
       if (e.Data[0] != 0xff)
       {
         int BNum = (int)Math.Round(e.Data[0] / 28.0, MidpointRounding.AwayFromZero) - 1;
-        Common.BrakeNotchNum = BNum >= 8 ? 99 : BNum;
+        if (isPBExc) Common.PowerNotchNum = BNum;
+        else Common.BrakeNotchNum = BNum >= BHandleMAX ? 99 : BNum;
       }
       if (e.Data[1] != 0xff)
-        Common.PowerNotchNum = (int)Math.Round(e.Data[1] / 18.0, MidpointRounding.AwayFromZero) - 1;
+      {
+        int PNum = (int)Math.Round(e.Data[1] / 18.0, MidpointRounding.AwayFromZero) - 1;
+        if (isPBExc) Common.BrakeNotchNum = PNum >= PHandleMAX ? 99 : PNum;
+        else Common.PowerNotchNum = PNum;
+      }
 
       bool[] keyState = Common.Ctrl_Key;
       keyState[0] = e.Data[2] != 0xff;//Horn SW
@@ -174,7 +185,8 @@ namespace TR.BIDSsvMOD.dgoCtrlUSB
       Console.WriteLine("communicate with dgocon\n" +
         " -a : (atc) set the atc panel number (default : null)" +
         " -m : (magnification) set the magnification number that multiply with ATC Panel value"+
-        " -n : (name) set the name of this connection");
+        " -n : (name) set the name of this connection"+
+        " -pbexc : (Power / Brake Exchange) Exchange roles of Power Handle and Brake Handle");
     }
   }
 }
